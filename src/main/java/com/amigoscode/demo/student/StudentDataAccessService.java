f018f523b47e1f8f2090c3ad4a1208d5cae48f4d
Andriy Paliychuk <a.b.paliychuk@gmail.com>
2020-03-19 15:27:52 +0200

    conflict

--- src/main/java/com/amigoscode/demo/student/StudentDataAccessService.java
+++ src/main/java/com/amigoscode/demo/student/StudentDataAccessService.java
@@ -40,11 +40,7 @@ public class StudentDataAccessService {
                 " last_name, " +
                 " email, " +
                 " gender) " +
-<<<<<<< .merge_file_MQz6hr
-                "VALUES (?, ?, ?, ?, ?::gender)";
-=======
                 "VALUES (?, ?, ?, ?, ?::GENDER)";
->>>>>>> .merge_file_5aQmQp
         return jdbcTemplate.update(
                 sql,
                 studentId,
